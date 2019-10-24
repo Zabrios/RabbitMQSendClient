ffcdb420c4394e0ba190117ebb4fdb7d6162ff01
Daniel Ruz <Zabrios@users.noreply.github.com>
2019-10-24 08:23:59 +0200

    Update Alumno.cs

--- AlumnoLibrary/Alumno.cs
+++ AlumnoLibrary/Alumno.cs
@@ -8,18 +8,19 @@ namespace AlumnoLibrary
 {
     public class Alumno
     {
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public int Edad { get; set; }
+        
         public Alumno()
         {
         }
+        
         public Alumno(string nombre, string apellidos, int edad)
         {
             Nombre = nombre;
             Apellidos = apellidos;
             Edad = edad;
         }
-
-        public string Nombre { get; set; }
-        public string Apellidos { get; set; }
-        public int Edad { get; set; }
     }
 }
